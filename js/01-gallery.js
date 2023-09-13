0a228c85253bc4dd08d66710b33e15159fde6908
DanielJedrasik <djedrasik3@gmail.com>
2023-09-13 20:32:47 +0200

    done homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const fotoGallery = document.querySelector(".gallery");
+const getFotoGallery = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<div class="gallery__item"><a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a> </div>`
+  )
+  .join(" ");
+fotoGallery.insertAdjacentHTML("beforeend", getFotoGallery);
+function bigFoto(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const modal = basicLightbox.create(
+    `<img src="${event.target.dataset.source}"/>`
+  );
+  modal.show();
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      modal.close();
+    }
+  });
+}
+fotoGallery.addEventListener("click", bigFoto);
